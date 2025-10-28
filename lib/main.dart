df2fd0f3fc3b7567cbbcf9c7600d7b0b0257b150
nazmuldev23 <nazmulhossen23.bd@gmail.com>
2025-10-28 09:09:44 +0600

    Entry

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:e_commerce0/firebase_options.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ); // Android automatically reads google-services.json
   runApp(const MyApp());
 }
